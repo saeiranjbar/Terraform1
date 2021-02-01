f48dd6d3e33a5cb08fb53a8b0ab8dcb4b8fdbfd1
Saei Ranjbar <saeiranjbar@gmail.com>
2021-02-01 13:37:57 -0800

    changes to the conf file

--- main.tf
+++ main.tf
@@ -2,11 +2,27 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "Example" {
-  ami = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+resource "aws_instance" "Web-server" {
+    ami = "ami-03d6336a"
+    instance_type = "t2.micro"
 
-  tags = {
-    name = "Terraform-example"
+    tags = {
+      name = "Web Server"
+    }
+
+    user_data =  <<-EOF
+                  #!/bin/bash
+                  echo "Hello, World" > index.html
+                  nohup busybox httpd -f -p 8080 &
+                  EOF
+    vpc_security_group_ids = [aws_security_group.instance.id]
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
